0272dbcb3f1d951f134f0589041e79df0a7cb576
Massimiliano Fanciulli <fanciulli@gmail.com>
2016-02-18 18:56:18 +0100

    Updated  with checker

--- Plugin-Checker/index.js
+++ Plugin-Checker/index.js
@@ -1,1 +1,31 @@
+/**
+	Plugin checker
+**/
+var argv = require('minimist')(process.argv.slice(2));
+var winston = require('winston');
+
+var ccommand;
+
+ccommand.logger = new (winston.Logger)({
+		transports: [
+			new (winston.transports.Console)(),
+			new (winston.transports.File)({
+				filename: '/var/log/volumio.log',
+				json: false
+			})
+		]
+	});
+
+var pluginmanager=new(require(__dirname+'/pluginmanager.js'))(ccommand);
+
+if (argv.d) {
+	console.log('Testing plugin in folder '+argv.d);
+	
+//	pluginmanager.setPluginFolder(argv.d);
+//	pluginmanager.loadPlugins();
+
+}
+else console.log('Please specify the plugin folder through the -d option');
+
+
 
